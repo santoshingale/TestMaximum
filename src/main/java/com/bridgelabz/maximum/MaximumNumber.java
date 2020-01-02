4b10901072ba9d5b7db2ec3830a23f162e389ab3
santoshingale <santoshingale876@gmail.com>
2020-01-02 18:38:55 +0530

    Refactored: To create generic class which accept 3 generic variable

--- src/main/java/com/bridgelabz/maximum/MaximumNumber.java
+++ src/main/java/com/bridgelabz/maximum/MaximumNumber.java
@@ -1,9 +1,21 @@
 package com.bridgelabz.maximum;
 
-public class MaximumNumber {
+public class MaximumNumber < T extends Comparable > {
+    T first;
+    T second;
+    T third;
 
-    public static < E extends Comparable > E findMaximum(E first, E second, E third) {
+    public MaximumNumber(T first, T second, T third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
 
+    public T testMaximum() {
+        return testMaximum(first,second,third);
+    }
+
+    public static < E extends Comparable > E testMaximum(E first, E second, E third) {
         E max = first;
         if( second.compareTo(max) > 0 ) {
             max = second;
--- src/test/java/com/bridgelabz/testmaximum/TestMaximum.java
+++ src/test/java/com/bridgelabz/testmaximum/TestMaximum.java
@@ -11,7 +11,7 @@ public class TestMaximum {
         Integer first = 30;
         Integer second = 10;
         Integer third = 20;
-        Integer maximumNumber = MaximumNumber.findMaximum(first, second, third);
+        Integer maximumNumber = MaximumNumber.testMaximum(first, second, third);
         Assert.assertEquals(first,maximumNumber);
     }
 
@@ -20,7 +20,7 @@ public class TestMaximum {
         Integer first = 30;
         Integer second = 40;
         Integer third = 20;
-        Integer maximumNumber = MaximumNumber.findMaximum(first, second, third);
+        Integer maximumNumber = MaximumNumber.testMaximum(first, second, third);
         Assert.assertEquals(second,maximumNumber);
     }
 
@@ -29,7 +29,7 @@ public class TestMaximum {
         Integer first = 30;
         Integer second = 10;
         Integer third = 40;
-        Integer maximumNumber = MaximumNumber.findMaximum(first, second, third);
+        Integer maximumNumber = MaximumNumber.testMaximum(first, second, third);
         Assert.assertEquals(third,maximumNumber);
     }
 
@@ -38,7 +38,7 @@ public class TestMaximum {
         Float first = 10.50f;
         Float second = 10.40f;
         Float third = 10.20f;
-        Float maximumNumber = MaximumNumber.findMaximum(first, second, third);
+        Float maximumNumber = MaximumNumber.testMaximum(first, second, third);
         Assert.assertEquals(first,maximumNumber);
     }
 
@@ -47,7 +47,7 @@ public class TestMaximum {
         Float first = 10.10f;
         Float second = 10.40f;
         Float third = 10.20f;
-        Float maximumNumber = MaximumNumber.findMaximum(first, second, third);
+        Float maximumNumber = MaximumNumber.testMaximum(first, second, third);
         Assert.assertEquals(second,maximumNumber);
     }
 
@@ -56,25 +56,106 @@ public class TestMaximum {
         Float first = 10.10f;
         Float second = 10.20f;
         Float third = 10.30f;
-        Float maximumNumber = MaximumNumber.findMaximum(first, second, third);
+        Float maximumNumber = MaximumNumber.testMaximum(first, second, third);
         Assert.assertEquals(third,maximumNumber);
     }
 
     @Test
     public void whenGiven_threeString_ifFirstIsGreater_shouldReturnFirstString() {
-        String maximumStringValue = MaximumNumber.findMaximum("Peach", "Banana", "Apple");
+        String maximumStringValue = MaximumNumber.testMaximum("Peach", "Banana", "Apple");
         Assert.assertEquals("Peach", maximumStringValue);
     }
 
     @Test
     public void whenGiven_threeString_ifSecondIsGreater_shouldReturnSecondString() {
-        String maximumStringValue = MaximumNumber.findMaximum("Banana","Peach","Apple");
+        String maximumStringValue = MaximumNumber.testMaximum("Banana","Peach","Apple");
         Assert.assertEquals("Peach", maximumStringValue);
     }
 
     @Test
     public void whenGiven_threeString_ifThirdIsGreater_shouldReturnThirdString() {
-        String maximumStringValue = MaximumNumber.findMaximum("Banana","Apple","Peach");
+        String maximumStringValue = MaximumNumber.testMaximum("Banana","Apple","Peach");
         Assert.assertEquals("Peach", maximumStringValue);
+    }
+
+    @Test
+    public void whenGiven_threeIntegerNumber_ifFirstIsGreater_byUsingGeneric_shouldReturnFirstNumber() {
+        Integer first = 30;
+        Integer second = 10;
+        Integer third = 20;
+        MaximumNumber maximumNumber = new MaximumNumber(first, second, third);
+        Comparable comparable = maximumNumber.testMaximum();
+        Assert.assertEquals(first,comparable);
+    }
+
+    @Test
+    public void whenGiven_threeIntegerNumber_ifSecondIsGreater_byUsingGeneric_shouldReturnSecondNumber() {
+        Integer first = 30;
+        Integer second = 40;
+        Integer third = 20;
+        MaximumNumber maximumNumber = new MaximumNumber(first, second, third);
+        Comparable comparable = maximumNumber.testMaximum();
+        Assert.assertEquals(second,comparable);
+    }
+
+    @Test
+    public void whenGiven_threeIntegerNumber_ifThirdIsGreater_byUsingGeneric_shouldReturnThirdNumber() {
+        Integer first = 30;
+        Integer second = 10;
+        Integer third = 40;
+        MaximumNumber maximumNumber = new MaximumNumber(first, second, third);
+        Comparable comparable = maximumNumber.testMaximum();
+        Assert.assertEquals(third,comparable);
+    }
+
+    @Test
+    public void whenGiven_threeFloatNumber_ifFirstIsGreater_byUsingGeneric_shouldReturnFirstNumber() {
+        Float first = 10.50f;
+        Float second = 10.40f;
+        Float third = 10.20f;
+        MaximumNumber maximumNumber = new MaximumNumber(first, second, third);
+        Comparable comparable = maximumNumber.testMaximum();
+        Assert.assertEquals(first,comparable);
+    }
+
+    @Test
+    public void whenGiven_threeFloatNumber_ifSecondIsGreater_byUsingGeneric_shouldReturnSecondNumber() {
+        Float first = 10.10f;
+        Float second = 10.40f;
+        Float third = 10.20f;
+        MaximumNumber maximumNumber = new MaximumNumber(first, second, third);
+        Comparable comparable = maximumNumber.testMaximum();
+        Assert.assertEquals(second,comparable);
+    }
+
+    @Test
+    public void whenGiven_threeFloatNumber_ifThirdIsGreater_byUsingGeneric_shouldReturnThirdNumber() {
+        Float first = 10.10f;
+        Float second = 10.20f;
+        Float third = 10.30f;
+        MaximumNumber maximumNumber = new MaximumNumber(first, second, third);
+        Comparable comparable = maximumNumber.testMaximum();
+        Assert.assertEquals(third,comparable);
+    }
+
+    @Test
+    public void whenGiven_threeString_ifFirstIsGreater_byUsingGeneric_shouldReturnFirstString() {
+        MaximumNumber maximumNumber = new MaximumNumber("Peach", "Banana", "Apple");
+        Comparable comparable = maximumNumber.testMaximum();
+        Assert.assertEquals("Peach",comparable);
+    }
+
+    @Test
+    public void whenGiven_threeString_ifSecondIsGreater_byUsingGeneric_shouldReturnSecondString() {
+        MaximumNumber maximumNumber = new MaximumNumber("Banana","Peach","Apple");
+        Comparable comparable = maximumNumber.testMaximum();
+        Assert.assertEquals("Peach",comparable);
+    }
+
+    @Test
+    public void whenGiven_threeString_ifThirdIsGreater_byUsingGeneric_shouldReturnThirdString() {
+        MaximumNumber maximumNumber = new MaximumNumber("Banana","Apple","Peach");
+        Comparable comparable = maximumNumber.testMaximum();
+        Assert.assertEquals("Peach",comparable);
     }
 }
